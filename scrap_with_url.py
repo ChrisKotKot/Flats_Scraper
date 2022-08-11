4735921c985b4d618262b62e53201d9f0c6a7f5d
Chris <kotkotarskikot@gmail.com>
2022-08-11 12:03:48 +0200

    -- working with getting url(scrap_with_url.py) --

--- scrap_with_url.py
+++ scrap_with_url.py
@@ -10,25 +10,22 @@ def parse_page(number):
 	print(f'Pracuje nad stroną numer {number}.')
 	page = get(f'{URL}?page={number}')
 	bs = BeautifulSoup(page.content, "html.parser")
-	for offer in bs.find_all('div', class_='css-9nzgu8'):
+	for offer in bs.find_all('div', attrs={'data-cy': 'l-card', 'class': ['css-19ucd76']}):
 		name = offer.find('h6', class_='css-v3vynn-Text eu5v0x0').get_text().strip()
 		price = parse_price(offer.find('p', class_='css-wpfvmn-Text eu5v0x0').get_text())
 		location = offer.find('p', class_='css-p6wsjo-Text eu5v0x0').get_text().strip().split('-')[0]
 		time_added = offer.find('p', class_='css-p6wsjo-Text eu5v0x0').get_text().strip().split('-')[1]
-		# link = offer.find('a href', class_='css-1bbgabe')
-		# link = offer.find('a')
-		# print(link)
-
-		cursor.execute('INSERT INTO offers VALUES(?,?,?,?)', (name, price, location, time_added))
+		link = offer.find('a')
+		cursor.execute('INSERT INTO offers VALUES(?,?,?,?,?)', (name, price, location, time_added,link['href']))
 
 		db.commit()
 
 URL = 'https://www.olx.pl/d/nieruchomosci/mieszkania/sprzedaz/podlaskie/'
-db = sqlite3.connect('dane1.db')
+db = sqlite3.connect('dane-link.db')
 cursor = db.cursor()
 
 if len(argv) > 1 and argv[1] == 'setup':
-	cursor.execute('''CREATE TABLE offers (name TEXT, price REAL, city TEXT, date TEXT )''')
+	cursor.execute('''CREATE TABLE offers (name TEXT, price REAL, city TEXT, date TEXT,URL TEXT )''')
 	quit()
 #python main.py setup
 
